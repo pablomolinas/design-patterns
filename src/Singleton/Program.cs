5c9fc4549eb871235efcd37776e31480ea3a5de4
JuanPabloMolinas <JuanPabloMolinas@STANDARG0074>
2023-11-05 19:30:00 -0300

    Agregar archivos de proyecto.

---
+++ src/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton
+{
+    public static class Application
+    {
+        public static void Main(string[] args)
+        {
+            var instance1 = Singleton.Instance();
+            var instance2 = Singleton.Instance();
+            var instance3 = Singleton.Instance();
+        }
+    }
+}
---
+++ src/Singleton/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        protected Singleton()
+        {
+            Console.WriteLine("Instance created!");
+        }
+
+        public static Singleton Instance()
+        {
+            if (_instance is null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+    }
+}
